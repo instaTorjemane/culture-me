4276b89d241932d65c2f65b439debc05ed76994c
bah.studies@gmail.com <bah.studies@gmail.com@0fdb4100-51ec-fe4c-e76d-770b88d78a57>
2014-06-12 14:40:57 +0000

    UserDAO : modification de la méthode addUser

--- branches/culture-me/src/main/java/fr/upmc/aar/dao/UserDAO.java
+++ branches/culture-me/src/main/java/fr/upmc/aar/dao/UserDAO.java
@@ -9,19 +9,48 @@ import fr.upmc.aar.model.User;
 
 public class UserDAO {
 	
-	public static void addUser(User user){
-		PersistenceManager pm = PMF.get().getPersistenceManager();
-		pm.makePersistent(user);
-		pm.close();
+	public static boolean addUser(User user)
+	{
+		boolean state = false;
+		if(!userExists(user.getUsername())){
+			PersistenceManager pm = PMF.get().getPersistenceManager();
+	        try {
+	            pm.makePersistent(user);
+	            state = true;
+	        } 
+	        catch(Exception e){
+	        	state =  false;
+	        }
+	        finally {
+	            pm.close();
+	        }
+		}
+		else{
+			state = false;
+		}
+		return state;
 	}
 	
-	public static List<User> listUser(){
+	public static List<User> listUser()
+	{
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 		Query q = pm.newQuery(User.class);
 		@SuppressWarnings("unchecked")
 		List<User> users = (List<User>) q.execute();
 		pm.close();
 		return users;
+	}
+	
+	
+	public static boolean userExists(final String username)
+	{
+		boolean exists = false;
+		
+		@SuppressWarnings("unchecked")
+		List<User> users = (List<User>) PMF.get().getPersistenceManager().newQuery(User.class, "(username == '" + username + "')").execute();
+		exists =  (users!=null && users.size() > 0);
+		
+		return exists;
 	}
 
 }
