46112ab326df105775d57c388bd6d29cd075a73b
shazad.rsk@gmail.com <shazad.rsk@gmail.com@0fdb4100-51ec-fe4c-e76d-770b88d78a57>
2014-06-18 15:17:09 +0000

    -- All package-info.java deleted

--- branches/culture-me/src/main/java/fr/upmc/aar/model/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains the <a href="https://code.google.com/p/google-gson/"
- *            target="_blank"> Gson</a> value objects used to encapsulate the data obtained querying the API.
- * @author <a href="mailto:tambug@gmail.com">Giordano Tamburrelli</a>
- */
-package fr.upmc.aar.model;
-
