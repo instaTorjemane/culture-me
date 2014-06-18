d7c86434ca329a2128f55faba9112b42bb97d9a6
shazad.rsk@gmail.com <shazad.rsk@gmail.com@0fdb4100-51ec-fe4c-e76d-770b88d78a57>
2014-06-18 15:17:09 +0000

    -- All package-info.java deleted

--- src/main/java/fr/upmc/aar/jtomato/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains a Java client for the <a href="http://developer.rottentomatoes.com"
- *            target="_blank"> Rotten Tomatoes API v 1.0</a>.
- * @author <a href="mailto:tambug@gmail.com">Giordano Tamburrelli</a>
- */
-package fr.upmc.aar.jtomato;
-
