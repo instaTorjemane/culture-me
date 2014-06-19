3d723104d67ec93cf9de48c4e555c8e8a633a2f4
bah.studies@gmail.com <bah.studies@gmail.com@0fdb4100-51ec-fe4c-e76d-770b88d78a57>
2014-06-19 08:09:42 +0000

    rajout dans MovieDAO de la méthode de récupération des movies

---
+++ branches/culture-me/src/main/java/fr/upmc/aar/servlet/MovieListServlet.java
@@ -0,0 +1,54 @@
+package fr.upmc.aar.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.upmc.aar.dao.MovieDAO;
+import fr.upmc.aar.dao.UserDAO;
+import fr.upmc.aar.model.Movie;
+import fr.upmc.aar.model.User;
+
+/**
+ * Servlet implementation class MovieListServlet
+ */
+public class MovieListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MovieListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Movie> movies = MovieDAO.listMovie();
+		response.getWriter().print("<html> "+ "<body><h1>List des movies</h1>");
+		
+		for(Movie movie : movies){
+			response.getWriter().print("<p>titre : " + movie.getTitle() + "</p>" + 
+					"<p>Description : </p><p>" + movie.getSynopsis() +  "</p>" + 
+					"<p>sortie : " + movie.getRuntime() + "</p>" 
+					);
+		}
+		
+		response.getWriter().print("</body>" + "</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
