93aeaf81c27cd91c8725d2cfbdeb8c711a86bb53
bah.studies@gmail.com <bah.studies@gmail.com@0fdb4100-51ec-fe4c-e76d-770b88d78a57>
2014-06-19 08:09:42 +0000

    rajout dans MovieDAO de la méthode de récupération des movies

--- src/main/java/fr/upmc/aar/dao/MovieDAO.java
+++ src/main/java/fr/upmc/aar/dao/MovieDAO.java
@@ -1,6 +1,9 @@
 package fr.upmc.aar.dao;
 
+import java.util.List;
+
 import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
 
 import fr.upmc.aar.model.Movie;
 
@@ -23,6 +26,16 @@ public class MovieDAO {
 			pm.close();
 		}
 
+	}
+	
+	public static List<Movie> listMovie()
+	{
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		Query q = pm.newQuery(Movie.class);
+		@SuppressWarnings("unchecked")
+		List<Movie> movies = (List<Movie>) q.execute();
+		pm.close();
+		return movies;
 	}
 
 
