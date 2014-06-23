32a8b22ac57418cf77465cc74aa439bdeda5b820
shazad.rsk@gmail.com <shazad.rsk@gmail.com@0fdb4100-51ec-fe4c-e76d-770b88d78a57>
2014-06-23 17:41:29 +0000

    -- Servlet qui envoie le mot de passe oublié de l'utilisateur par mail -- L'identifiant de récupération est le mail -- Nouvelle méthode crée dans UserDAO()

--- src/main/java/fr/upmc/aar/servlet/NewAccountMailServlet.java
+++ src/main/java/fr/upmc/aar/servlet/NewAccountMailServlet.java
@@ -41,37 +41,41 @@ public class NewAccountMailServlet extends HttpServlet {
 		//UserDAO.getUser ==> Par mail? Par Id?
 		User u = UserDAO.getUser("shazad","shazad");
 		
+		if(u != null){
+			Properties props = new Properties();
+			Session session = Session.getDefaultInstance(props, null);
+			
+			String msgBody = "<html><body>"
+					+ "<h2> Bienvenue chez culture-me "+u.getUsername()+"!</h2>"
+					+ "<p> Vous êtes maintenant inscrit sur le premier site d'évaluations de produits culturels !</p>"
+					+ "<p> Participez vous aussi, et donnez votre avis sur les films, jeux videos et livres que vous avez aimez (ou pas!) !</p>"
+					+ "<br/>"
+					+ "<p> En cas de problème avec le site ou pour nous donner des idées d'améliorations, contactez notre <a href=\"mailto:stl.insta.aar@gmail.com\">webmaster</a></p>"
+					+ "<p> A bientôt sur Culture-me ! </p>"
+					+ "<body></html>";
+			
+			try {
+			    Message msg = new MimeMessage(session);
+			    msg.setContent(msgBody, "text/html");
+			    msg.setFrom(new InternetAddress("stl.insta.aar@gmail.com", "Webmaster Culture-me"));
+			    msg.addRecipient(Message.RecipientType.TO,new InternetAddress(u.getMail(),"M. "+u.getLastName()));
+			    msg.setSubject("[Culture-me] Bienvenue "+u.getUsername()+" !");
+			    //msg.setText(msgBody);
+			    Transport.send(msg);
+			    
+			    /*Test affichage corps OK
+			     * response.getWriter().print("<h1>Envoi d'Email</h1><p>From: stl.insta.aar@gmail.com</p>"
+			    		+ "<p>To: "+u.getMail()+"</p><p>Corps du mail: <br />"+msgBody+"</p><br/><p>Message bien envoyé!</p>");
+			    */
+			    }catch (AddressException e) {
+			    	response.getWriter().print(" AddressException" +e.getMessage());
+			    }catch (MessagingException e) {
+			    	response.getWriter().print(" MessagingException" +e.getMessage());
+			    }
+			
+			
+		}
 		
-		Properties props = new Properties();
-		Session session = Session.getDefaultInstance(props, null);
-		
-		String msgBody = "<html><body>"
-				+ "<h2> Bienvenue chez culture-me "+u.getUsername()+"!</h2>"
-				+ "<p> Vous êtes maintenant inscrit sur le premier site d'évaluations de produits culturels !</p>"
-				+ "<p> Participez vous aussi, et donnez votre avis sur les films, jeux videos et livres que vous avez aimez (ou pas!) !</p>"
-				+ "<br/>"
-				+ "<p> En cas de problème avec le site ou pour nous donner des idées d'améliorations, contactez notre <a href=\"mailto:stl.insta.aar@gmail.com\">webmaster</a></p>"
-				+ "<p> A bientôt sur Culture-me ! </p>"
-				+ "<body></html>";
-		
-		try {
-		    Message msg = new MimeMessage(session);
-		    msg.setContent(msgBody, "text/html");
-		    msg.setFrom(new InternetAddress("stl.insta.aar@gmail.com", "Webmaster Culture-me"));
-		    msg.addRecipient(Message.RecipientType.TO,new InternetAddress(u.getMail(),"M. "+u.getLastName()));
-		    msg.setSubject("[Culture-me] Bienvenue "+u.getUsername()+" !");
-		    //msg.setText(msgBody);
-		    Transport.send(msg);
-		    
-		    /*Test affichage corps OK
-		     * response.getWriter().print("<h1>Envoi d'Email</h1><p>From: stl.insta.aar@gmail.com</p>"
-		    		+ "<p>To: "+u.getMail()+"</p><p>Corps du mail: <br />"+msgBody+"</p><br/><p>Message bien envoyé!</p>");
-		    */
-		    }catch (AddressException e) {
-		    	response.getWriter().print(" AddressException" +e.getMessage());
-		    }catch (MessagingException e) {
-		    	response.getWriter().print(" MessagingException" +e.getMessage());
-		    }
 		
 	}
 
--- src/main/java/fr/upmc/aar/servlet/SendPasswordServlet.java
+++ src/main/java/fr/upmc/aar/servlet/SendPasswordServlet.java
@@ -1,10 +1,25 @@
 package fr.upmc.aar.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Dispatch;
+
+import fr.upmc.aar.dao.UserDAO;
+import fr.upmc.aar.model.User;
 
 /**
  * Servlet implementation class SendPasswordServlet
@@ -24,14 +39,66 @@ public class SendPasswordServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String email = request.getParameter("emailcheck");
+		String EMAIL_REGEX = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+		boolean validmail = email.matches(EMAIL_REGEX);
+		String password = "";
+		
+		if(validmail){
+			User u = UserDAO.getUserByMail(email);
+			
+			if(u != null){
+				password = u.getPassword();
+				
+				//Envoi du mail
+				Properties props = new Properties();
+				Session session = Session.getDefaultInstance(props, null);
+				
+				String msgBody = "<html><body>"
+						+ "<h2> Récupération du mot de passe de "+u.getUsername()+"</h2>"
+						+ "<p> Vous avez fait une demande de mot de passe récemment. Vous trouverez votre mot de passe dans cet email.</p>"
+						+ "Votre mot de passe est : "+ password
+						+ "<br/>"
+						+ "<p> En cas de problème avec le site ou pour nous donner des idées d'améliorations, contactez notre <a href=\"mailto:stl.insta.aar@gmail.com\">webmaster</a></p>"
+						+ "<p> A bientôt sur Culture-me ! </p>"
+						+ "<body></html>";
+				
+				try {
+				    Message msg = new MimeMessage(session);
+				    msg.setContent(msgBody, "text/html");
+				    msg.setFrom(new InternetAddress("stl.insta.aar@gmail.com", "Webmaster Culture-me"));
+				    msg.addRecipient(Message.RecipientType.TO,new InternetAddress(u.getMail(),"M. "+u.getLastName()));
+				    msg.setSubject("[Culture-me] Récupération de mot de passe");
+				    //msg.setText(msgBody);
+				    Transport.send(msg);
+				    
+				    /*Test affichage corps OK
+				      response.getWriter().print("<h1>"+msg.getSubject()+"</h1><p>From: stl.insta.aar@gmail.com</p>"
+				    		+ "<p>To: "+u.getMail()+"</p><p>Corps du mail: <br />"+msgBody+"</p><br/><p>Message bien envoyé!</p>");
+				    */
+				    }catch (AddressException e) {
+				    	response.getWriter().print(" AddressException" +e.getMessage());
+				    }catch (MessagingException e) {
+				    	response.getWriter().print(" MessagingException" +e.getMessage());
+				    }
+			}
+		}else{
+			PrintWriter out = response.getWriter();
+			out.print("<div align='center'>Le mail est incorrect!</div>");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp"); 
+			dispatcher.forward(request, response);				
+		}
+		
+		
+		
+		
 	}
 
 }
